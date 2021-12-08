3e0c0221b543a26a076fa86a9c987ed09ccbb4f4
PattyCutie <iamsureiamcute@gmail.com>
2021-12-08 11:15:17 +0000

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,34 @@
-from machinetranslation import translator
+"""
+This is Finanal assignment: Python Web Application Creation and Deployment
+"""
 from flask import Flask, render_template, request
-import json
+from machinetranslation import translator
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+def english_to_french():
+    ''' english_to_french endpoint '''
+    text_to_translate = request.args.get('text_to_translate')
     # Write your code here
-    return "Translated text to French"
+    french_text = translator.english_to_french(text_to_translate)
+    # return "Translated text to French"
+    return french_text
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+def french_to_english():
+    ''' french_to_english endpoint '''
+    text_to_translate = request.args.get('text_to_translate')
     # Write your code here
-    return "Translated text to English"
+    english_text = translator.french_to_english(text_to_translate)
+    # return "Translated text to English"
+    return english_text
 
 @app.route("/")
-def renderIndexPage():
+def render_index_page():
+    ''' render page index.html ('/') '''
     # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
